1d8b3ba400fc194f58d16f75a62ad26d4e036cde
Parsapt <90087314+Parsa-PT@users.noreply.github.com>
2024-10-28 00:01:09 +0330

    Add files via upload

---
+++ constant/index.ts
@@ -0,0 +1,32 @@
+export const EVERY = [
+    {
+        title:'Visual reports',
+        icon:'/img/vector.svg',
+        desc:'Visual insights into your site’s performance.'
+    },
+    {
+        title:'SEO goal setting',
+        icon:'/img/vector.svg',
+        desc:'Helps you set and achieve SEO goals with guided assistance.'
+    },
+    {
+        title:'One-click optimization',
+        icon:'/img/vector.svg',
+        desc:'Perform complex SEO audits and optimizations with a single click.'
+    },
+    {
+        title:'Smart Keyword Generator',
+        icon:'/img/vector.svg',
+        desc:'Automatic suggestions and the best keywords to target.'
+    },
+    {
+        title:'Automated alerts',
+        icon:'/img/vector.svg',
+        desc:'Automatic notifications about your SEO health, including quick fixes.'
+    },
+    {
+        title:'Competitor reports',
+        icon:'/img/vector.svg',
+        desc:'Provides insights into competitors’ keyword strategies and ranking.'
+    },
+]
