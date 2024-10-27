dbf58fb72fd0aff24a516a172c14c7556f2a3a8d
Parsapt <90087314+Parsa-PT@users.noreply.github.com>
2024-10-27 23:59:14 +0330

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 
+import Client from "@/components/Client";
+import Every from "@/components/Every";
 import HomeSec from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
@@ -8,6 +10,8 @@ export default function Home() {
     <div  className=" max-w-[100%] overflow-x-hidden">
 
       <HomeSec/>
+      <Every/>
+      <Client/>
     </div>
   );
 }
